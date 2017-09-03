6182eedc518f83455ed86e3c4b7a9ebd3c5b61e4
dnxbjyj <dnxbjyj@126.com>
2017-09-03 16:33:18 +0800

    add basemap

---
+++ tools/pdb/sample/test1.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+import pdb
+s1 = 'aaa'
+pdb.set_trace()
+s2 = 'bbb'
+s3 = 'ccc'
+pdb.set_trace()
+s = s1 + s2 + s3
+print s
---
+++ tools/pdb/sample/test2.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+s1 = 'aaa'
+s2 = 'bbb'
+s3 = 'ccc'
+s = s1 + s2 + s3
+print s
